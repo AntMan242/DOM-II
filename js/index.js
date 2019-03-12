3768d5036fef17231b4df4f8fba875d89296e88f
anthony <anthony24.kwhomes@gmail.com>
2019-03-12 16:00:02 -0700

    Todays project.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+// mouseover
+const button = document.querySelector('.btn');
+
+button.addEventListener('mouseover', function() {
+    console.log('hovered over');
+});
+
+// keydown
+const navLink = document.querySelector('.nav-link');
+navLink.forEach(function() {
+    navLink.addEventListener('keydown', function(event) {
+        alert(`key down`);
+})
+
+});
+
+// wheel
+const allBody = document.querySelector('body');
+allBody.addEventListener('wheel', function() {
+    console.log('wheel');
+});
+
+// drag/drop
